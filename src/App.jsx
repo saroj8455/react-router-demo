4c28e2d26c3a30a69802d896e867b5489f2d1412
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-28 19:13:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Button } from 'primereact/button';
+
+function App() {
+  return (
+    <>
+      <h1>Router Demo Project</h1>
+      <div className='card flex justify-content-center'>
+        <Button label='Check' icon='pi pi-check' />
+      </div>
+    </>
+  );
+}
+
+export default App;
